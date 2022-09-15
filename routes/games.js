ac1a9d6dbf0f106a7683c8f7d372cd146d77b719
CoolRy <that007guy@hotmail.com>
2022-09-15 13:00:26 -0400

    fix decode token

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as gamesCtrl from '../controllers/games.js'
-// import { decodeFromToken, checkAuth } from '../middleware/auth.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
 
